5ded8b22ba56184c886ec7eb4053dfb115586635
ralmosara <138684817+ralmosara@users.noreply.github.com>
2025-03-15 01:15:23 -0300

    Update Products
    Update Products

--- project-name/src/products/products.controller.ts
+++ project-name/src/products/products.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
-
+import { ProductsService } from './products.service';
 @Controller('products')
 export class ProductsController {
+    constructor(private ProductService: ProductsService) {
+
+    }
+
+
     @Get()
     getProducts(): any {
         return  {products:'All Products'};
+
     }
 
+    @Post()
+    addProducts(
+        @Body('title') pTitle:string,
+        @Body('description') pDescription:string,
+        @Body('price') pPrice:number,
+
+    ){
+        const returnedId = this.ProductService.insertProduct(pTitle,pDescription,pPrice);
+
+        return {id:returnedId};
+    }
   
 
 }
--- project-name/src/products/products.service.ts
+++ project-name/src/products/products.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './products.model';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    product: Product [] = [];
+    
+    insertProduct(title:string,description:string,price:number) {
+        const id = new Date().toString();
+        const newProduct = new Product(id,title,description,price);
+        this.product.push(newProduct);
+        return id;
+    }
+}
